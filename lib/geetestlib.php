c3efc3b33245a5cdce65543fbe8c5f434afee6ab
tanxu1993 <877077145@qq.com>
2015-04-23 11:16:25 +0800

    Revert "重构核心类和发布至 composer "

--- lib/geetestlib.php
+++ lib/geetestlib.php
@@ -3,36 +3,33 @@
  * 极验行为式验证安全平台，php 网站主后台包含的库文件
  */
 
-class Geetest{
+define('GT_API_SERVER', 'http://api.geetest.com');
+define('GT_SSL_SERVER', 'https://api.geetest.com');
 
-	const GT_API_SERVER  = 'http://api.geetest.com';
-	const GT_SSL_SERVER  = 'http://api.geetest.com';
-	const GT_SDK_VERSION  = 'php_2.15.4.2.2';
+define('GT_SDK_VERSION', 'php_2.15.4.2.2');
 
-	private $captcha_id;
-	private $private_key;
-
-	public function __construct() {
+class GeetestLib{
+	function __construct() {
 		$this->challenge = "";
 	}
 
-	public function set_captchaid($captcha_id) {
+	function set_captchaid($captcha_id) {
 		$this->captcha_id = $captcha_id;
 	}
 
-	public function set_privatekey($private_key) {
+	function set_privatekey($private_key) {
 		$this->private_key = $private_key;
 	}
-
-	public function register() {
-		$this->challenge = $this->send_request("/register.php", array("gt"=>$this->captcha_id));
+	
+	function register() {
+		$this->challenge = $this->_send_request("/register.php", array("gt"=>$this->captcha_id));
 		if (strlen($this->challenge) != 32) {
 			return 0;
 		}
 		return 1;
 	}
-
-	public function get_widget($product, $popupbtnid = "", $ssl = FALSE) {
+	
+	function get_widget($product, $popupbtnid="", $ssl=FALSE) {
 		$params = array(
 			"gt" => $this->captcha_id,
 			"challenge" => $this->challenge,
@@ -41,37 +38,37 @@ class Geetest{
 		if ($product == "popup") {
 			$params["popupbtnid"] = $popupbtnid;
 		}
-		$server = $ssl ? self::GT_SSL_SERVER : self::GT_API_SERVER;
-		return "<script type='text/javascript' src='". $server ."/get.php?". http_build_query($params) ."'></script>";
+		$server = $ssl ? GT_SSL_SERVER : GT_API_SERVER;
+		return "<script type='text/javascript' src='".$server."/get.php?".http_build_query($params)."'></script>";
 	}
 
-	public function validate($challenge, $validate, $seccode) {
-		if ( ! $this->check_validate($challenge, $validate)) {
+	function validate($challenge, $validate, $seccode) {	
+		if ( ! $this->_check_validate($challenge, $validate)) {
 			return FALSE;
 		}
-		$query = http_build_query(array("seccode"=>$seccode, "sdk"=>self::GT_SDK_VERSION));
-		$codevalidate = $this->http_post('api.geetest.com', '/validate.php', $query);
-		if (strlen($codevalidate) > 0 && $codevalidate == md5($seccode)) {
+		$query = http_build_query(array("seccode"=>$seccode,"sdk"=>GT_SDK_VERSION));
+		$codevalidate = $this->_http_post('api.geetest.com', '/validate.php', $query);
+		if (strlen($codevalidate)>0 && $codevalidate==md5($seccode)) {
 			return TRUE;
 		} else if ($codevalidate == "false"){
 			return FALSE;
-		} else {
+		} else { 
 			return $codevalidate;
 		}
 	}
-
-	private function check_validate($challenge, $validate) {
+	
+	function _check_validate($challenge, $validate) {
 		if (strlen($validate) != 32) {
 			return FALSE;
 		}
 		if (md5($this->private_key.'geetest'.$challenge) != $validate) {
 			return FALSE;
-		}
+		} 
 		return TRUE;
 	}
 
-	private function send_request($path, $data, $method = "GET") {
-		if ($method == "GET") {
+	function _send_request($path, $data, $method="GET") {
+		if ($method=="GET") {
 			$opts = array(
 			    'http'=>array(
 				    'method'=>"GET",
@@ -79,12 +76,13 @@ class Geetest{
 			    )
 		    );
 		    $context = stream_context_create($opts);
-			$response = file_get_contents(self::GT_API_SERVER.$path."?". http_build_query($data), false, $context);
-			return $response;
+			$response = file_get_contents(GT_API_SERVER.$path."?".http_build_query($data), false, $context);
+
 		}
+		return $response;
 	}
 
-	private function http_post($host,$path,$data,$port = 80){
+	function _http_post($host,$path,$data,$port = 80){
 		$http_request = "POST $path HTTP/1.0\r\n";
 		$http_request .= "Host: $host\r\n";
 		$http_request .= "Content-Type: application/x-www-form-urlencoded\r\n";
